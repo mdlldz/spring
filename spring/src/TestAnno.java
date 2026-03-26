5f44dd9cf597c16c94d1b784bf76c6ba490b4660
mdlldz <18312315376@qq.com>
2026-03-26 17:31:00 +0800

    spring

---
+++ spring/src/TestAnno.java
@@ -0,0 +1,10 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TestAnno {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        User user = context.getBean(User.class);
+        System.out.println(user.getName());
+    }
+}
---
+++ spring/src/User0.java
@@ -0,0 +1,10 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class User0 {
+    private String name = "Annotation User";
+
+    public String getName() {
+        return name;
+    }
+}
