cd3cdfdb3533a60b20814b28a3dfc1fab11b238e
mdlldz <18312315376@qq.com>
2026-03-27 16:22:43 +0800

    Correct print statement in TestSpring.java
    Fix typo in print statement for testing.

--- spring/src/TestSpring.java
+++ spring/src/TestSpring.java
@@ -3,7 +3,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        System.Out.Print("推送测试");
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         User user = (User) context.getBean("user");
         user.sayHello();
