7d2d53248e88a370e4ce94ee0e648ed5d1d5425c
mdlldz <18312315376@qq.com>
2026-03-27 15:52:00 +0800

    Add push test print statement in TestSpring
    Add a print statement for push test in main method.

--- spring/src/TestSpring.java
+++ spring/src/TestSpring.java
@@ -3,6 +3,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
+        System.out.Print("这是一个推送测试")
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         User user = (User) context.getBean("user");
         user.sayHello();
